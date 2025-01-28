9fe6bdd6807cc7544791a05e9234765dae49a436
samantha moss <samantha.moss87@aol.com>
2025-01-28 19:41:11 +0000

    Added Testcases for all 5 pages to check all is working

--- thai_massage/booking/tests.py
+++ thai_massage/booking/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+
+class BookingViewsTestCase(TestCase):
+    def test_home_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_treatments_page(self):
+        response = self.client.get(reverse('treatments'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'treatments.html')
+
+    def test_book_now_page(self):
+        response = self.client.get(reverse('book_now'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'book_now.html')
+
+    def test_contact_page(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')
+
+    def test_login_page(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
