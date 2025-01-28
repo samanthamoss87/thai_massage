5246de5e99242a983e704b6be7fcef34ad8dc729
samantha moss <samantha.moss87@aol.com>
2025-01-28 19:39:27 +0000

    Added treatment, contact, book_now, login views and urls

--- thai_massage/booking/views.py
+++ thai_massage/booking/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Home Page
 def home(request):
     return render(request, 'home.html')
+
+# Treatments Page
+def treatments(request):
+    return render(request, 'treatments.html')
+
+# Book Now Page
+def book_now(request):
+    return render(request, 'book_now.html')
+
+# Contact Page
+def contact(request):
+    return render(request, 'contact.html')
+
+# Login Page
+def login_view(request):
+    return render(request, 'login.html')
