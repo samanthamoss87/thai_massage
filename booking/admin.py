ac34a8059e3483dd7ac1277fb9d499daba4be19e
samantha moss <samantha.moss87@aol.com>
2025-02-02 19:12:08 +0000

    Moved django project to the root of the repository

---
+++ booking/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Treatments, Booking, UserProfile
+
+
+
+admin.site.register(Treatments)
+admin.site.register(Booking)
+admin.site.register(UserProfile)
