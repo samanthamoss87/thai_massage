bfcbd4d5803420afbfdadc755d5f8517e57e2867
samantha moss <samantha.moss87@aol.com>
2025-06-18 21:22:42 +0000

    removed cast=int from database port

--- thai_massage/settings.py
+++ thai_massage/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', default='5432', cast=int),
+        'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
 
